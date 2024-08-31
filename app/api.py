86d89f4c0dda40edb9471b0a629eda428d56bb60
choonggi <148122625+BlurSotong1@users.noreply.github.com>
2024-08-31 23:44:49 +0800

    reformatted the file

--- app/api.py
+++ app/api.py
@@ -1,24 +1,16 @@
 import flask
 from flask import request, jsonify
 from app.ntuc import search as searchNTUC
-from app.cold_storage import search as searchColdStorage
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['POST'])
-def queryAll():
-    resultsColdStorage = searchColdStorage(request.get_json()['query']) 
-    resultsNTUC = searchNTUC(request.get_json()['query'])
-
-    resultsCombined = resultsColdStorage + resultsNTUC
-
-    return jsonify({"results": resultsCombined})
+def queryNTUC():
+    return jsonify({"results": searchNTUC(request.get_json()['query'])})
 
 @app.route('/ntuc/', methods=['POST'])
 def queryNTUC():
     return jsonify({"results": searchNTUC(request.get_json()['query'])})
 
-@app.route('/cold-storage/', methods=['POST'])
-def queryColdStorage():
-    return jsonify({"results": searchColdStorage(request.get_json()['query'])})
--- app/ntuc.py
+++ app/ntuc.py
@@ -1,112 +1,96 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-from pandas.core.tools.numeric import to_numeric
 import ssl
 
 """
-	CONFIGURATION 
+    CONFIGURATION 
 """
-url = "https://www.fairprice.com.sg/search?query=" 
-headers = {'User-Agent': 'Mozilla/5.0'} 
+url = "https://www.fairprice.com.sg/search?query="
+headers = {'User-Agent': 'Mozilla/5.0'}
+
 
 # Takes in a search query and returns a list of items on NTUC Fairprice
-# 	that matches the query with their `title`, `price` and `link`
+# that matches the query with their `title`, `price`, `measurement`, and `link`
 
 # EXAMPLE:
-#	INPUT: 
-# 		"chicken breast whole"
-#	OUTPUT: 
-		# {
-		# 	"Aw's Market Chicken Breast Whole": {
-		# 		'price': 5.0, 
-		# 		'link': '/product/aw-s-market-chicken-breast-whole-400-g-90018633'	
-		# 	}, 
-		# 	"Aw's Market Duck Breast Whole": {
-		# 		'price': 9.45, 
-		# 		'link': '/product/aw-s-market-duck-breast-whole-800-g-90018554'}
-		#	}
-		#}
+# INPUT:
+#       "chicken breast whole"
+# OUTPUT:
+# [
+#     {
+#         'title': "Aw's Market Chicken Breast Whole",
+#         'price': 5.0,
+#         'measurement': '400 g',
+#         'link': 'https://www.fairprice.com.sg/product/aw-s-market-chicken-breast-whole-400-g-90018633',
+#         'supermarket': 'ntuc'
+#     },
+#     {
+#         'title': "Aw's Market Duck Breast Whole",
+#         'price': 9.45,
+#         'measurement': '800 g',
+#         'link': 'https://www.fairprice.com.sg/product/aw-s-market-duck-breast-whole-800-g-90018554',
+#         'supermarket': 'ntuc'
+#     }
+# ]
 def search(keywords):
-	context = ssl._create_unverified_context()
-	# a function to generate the search-query URL
-	def generateURL(url, keywords):
-		queryURL = url
-		cleanedKeywords = keywords.replace(' ', '%20')
-		queryURL = queryURL + cleanedKeywords
+    context = ssl._create_unverified_context()
 
-		return queryURL
-	
-	req = Request(generateURL(url, keywords), headers=headers)
-	page = urlopen(req,context=context)
-	html = page.read().decode("utf-8")
-	soup = BeautifulSoup(html, "html.parser")
+    # Function to generate the search-query URL
+    def generateURL(url, keywords):
+        cleanedKeywords = keywords.replace(' ', '%20')
+        queryURL = url + cleanedKeywords
+        return queryURL
 
-	matches = soup.select('div[class*="product-container"]')
+    req = Request(generateURL(url, keywords), headers=headers)
+    page = urlopen(req, context=context)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
 
-	# initialize the array to be returned
-	result = []
+    matches = soup.select('div[class*="product-container"]')
 
-	for elem in matches:
-		matches = elem.findChildren("a", recursive=False) 
-		
-		for match in matches:
-			"""
-				Step 1: Find the name of the food product
-			"""
-			# the image of the food product contain a 'title' attribute
-			#	that has the name of the food product
-			image = match.findChildren("img")
-			linkToProduct = match.attrs['href']
+    # Initialize the array to be returned
+    result = []
 
-			"""
-				Step 2: Find the price of the food product
-				
-					THE ISSUE OF MULTIPLE PRICES: 
-					Sometimes the product may be on discount, 
-						so 2 or more prices will be shown. 
+    for elem in matches:
+        matches = elem.findChildren("a", recursive=False)
 
-					To deal with this, we just return the lowest of 
-						all the prices; with the assumption that the lowest 
-						price is the discount price offered to the customer.
-			"""
-			# the span elements of the food product 
-			# 	contain its price
-			spanList = match.findChildren("span")
-			priceList = []
+        for match in matches:
+            # Step 1: Find the name of the food product
+            # The image of the food product contains a 'title' attribute that has the name of the food product
+            image = match.findChildren("img")
+            linkToProduct = match.attrs['href']
 
-			for span in spanList:
-				# check that the string contained inside the span element is referring to the 
-				# 	price of the food item (and not a button like "add to cart")
-				if ("$" in span.text):
-					cleanedPrice = (span.text).replace('$', '')
+            # Step 2: Find the price of the food product
+            # The span elements of the food product contain its price
+            spanList = match.findChildren("span")
+            priceList = []
 
-					priceList.append(((cleanedPrice)))
+            for span in spanList:
+                # Check that the string contained inside the span element is referring to the price of the food item
+                if "$" in span.text:
+                    cleanedPrice = span.text.replace('$', '')
+                    priceList.append(cleanedPrice)
 
-			"""
-				Step 3: Find the weight/volume of the food product
-			"""
-			units = [
-				'kg', 'KG',
-				'g', 'G',
-				'ml', 'ML'
-				'l', 'L'
-			]
+            # Step 3: Find the weight/volume of the food product
+            units = [
+                'kg', 'KG',
+                'g', 'G',
+                'ml', 'ML',
+                'l', 'L'
+            ]
 
-			measurement = ""
-			for span in spanList:
-				if (any(x in span.text for x in units) & (len(span.text) <= 15)):
-					measurement = span.text
+            measurement = ""
+            for span in spanList:
+                if any(x in span.text for x in units) and len(span.text) <= 15:
+                    measurement = span.text
 
-			"""
-				Step 4: Add the title, price, measurement and link to the food product to the result object
-			"""
-			result.append({
-				'title': image[0].attrs['title'],
-				'price': min(priceList), 
-				'measurement': measurement,
-				'link': "https://www.fairprice.com.sg" + linkToProduct,
-				'supermarket': 'ntuc'
-         	})
-					
-	return result
+            # Step 4: Add the title, price, measurement, and link to the food product to the result object
+            result.append({
+                'title': image[0].attrs['title'],
+                'price': min(priceList),
+                'measurement': measurement,
+                'link': "https://www.fairprice.com.sg" + linkToProduct,
+                'supermarket': 'ntuc'
+            })
 
+    return result
